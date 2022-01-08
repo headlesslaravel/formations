9a494465e08cf935283ce6aaddb7a12b437ce2df
Brian Dillingham <brian@dillingham.dev>
2022-01-08 12:30:32 -0500

    wip

--- tests/FilterTest.php
+++ tests/FilterTest.php
@@ -9,7 +9,6 @@ use HeadlessLaravel\Formations\Tests\Fixtures\Models\Comment;
 use HeadlessLaravel\Formations\Tests\Fixtures\Models\Like;
 use HeadlessLaravel\Formations\Tests\Fixtures\Models\Post;
 use HeadlessLaravel\Formations\Tests\Fixtures\Models\Tag;
-use HeadlessLaravel\Formations\Tests\Fixtures\Models\User;
 use HeadlessLaravel\Formations\Tests\Fixtures\PostFormation;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\DB;
@@ -936,8 +935,6 @@ class FilterTest extends TestCase
 
     public function test_calling_results_twice_is_cached()
     {
-        ray()->newScreen();
-        ray()->showQueries();
         $count = 0;
 
         DB::listen(function ($query) use (&$count) {
