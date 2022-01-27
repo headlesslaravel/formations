0038bc4a5fd2edb1e634f0b3e1bd75c91dbe1ed7
Mitul Golakiya <mtl.golakiya@gmail.com>
2022-01-27 15:41:17 +0530

    style: Apply fixes from StyleCI (#3)
    Co-authored-by: StyleCI Bot <bot@styleci.io>

--- src/Exports/Export.php
+++ src/Exports/Export.php
@@ -31,9 +31,9 @@ class Export implements FromCollection, WithHeadings
 
         $records = (new $this->model())->query()->with($eagerLoadings)->get();
         $result = collect([]);
-        foreach($records as $record) {
+        foreach ($records as $record) {
             $resultRecord = [];
-            foreach($this->fields as $field) {
+            foreach ($this->fields as $field) {
                 if ($field->isRelation()) {
                     $resultRecord[$field->key] = $record->{$field->internal}->{$field->relationColumn};
                 } else {
@@ -42,6 +42,7 @@ class Export implements FromCollection, WithHeadings
             }
             $result->add($resultRecord);
         }
+
         return $result;
     }
 
