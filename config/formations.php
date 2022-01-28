3fcf165c6e83406a4dbb2b6c2b1a2fb44c8ee6cd
Mitul Golakiya <mtl.golakiya@gmail.com>
2022-01-28 12:58:07 +0530

    feat: #76 new config temporary removed

--- config/formations.php
+++ config/formations.php
@@ -11,19 +11,4 @@ return [
     |
     */
     'mode' => 'blade',
-
-    'exports' => [
-
-        'date_format' => 'Ymd_his',
-
-        /*
-        |--------------------------------------------------------------------------
-        | Default file export format
-        |--------------------------------------------------------------------------
-        | Check here for possible values https://docs.laravel-excel.com/3.1/exports/export-formats.html
-        |
-        */
-        'file_format' => 'xlsx',
-
-    ]
 ];
