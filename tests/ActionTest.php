48485272abd16a0aeac46f1bdcff198a4a5d53b9
Mitul Golakiya <mtl.golakiya@gmail.com>
2022-02-08 09:25:54 +0530

    feat: #17 test for filters and sorts

--- tests/ActionTest.php
+++ tests/ActionTest.php
@@ -4,6 +4,7 @@ namespace HeadlessLaravel\Formations\Tests;
 
 use HeadlessLaravel\Formations\Tests\Fixtures\Jobs\SetStatus;
 use HeadlessLaravel\Formations\Tests\Fixtures\Models\Post;
+use HeadlessLaravel\Formations\Tests\Fixtures\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Queue;
 
@@ -86,6 +87,37 @@ class ActionTest extends TestCase
         Queue::assertPushed(SetStatus::class, function (SetStatus $job) use ($postIds) {
             return in_array($job->post->id, $postIds);
         });
+    }
+
+    public function test_actions_for_filters()
+    {
+        Queue::fake();
+
+        $user = $this->authUser();
+        $author = User::factory()->create(['name' => 'Hi author']);
+
+        $postOne = Post::factory()->create(['author_id' => $user->id, 'title' => 'Title 1']);
+        $postTwo = Post::factory()->create(['author_id' => $user->id, 'title' => 'Title 2']);
+        Post::factory(2)->create(['author_id' => $author->id]);
+
+        $response = $this->post(
+            'actions/posts/set-status',
+            ['selected' => 'all', 'fields' => ['status' => 'active'], 'author' => 1, 'sort-desc' => 'title']
+        )->assertOk();
+        $data = $response->json();
+        $this->assertArrayHasKey('id', $data);
+
+        $postIds = [$postOne->id, $postTwo->id];
+        Queue::assertPushed(SetStatus::class, 2);
+        Queue::assertPushed(SetStatus::class, function (SetStatus $job) use ($postIds) {
+            return in_array($job->post->id, $postIds);
+        });
+
+        /** @var SetStatus[] $jobs */
+        $jobs = Queue::pushedJobs()[SetStatus::class];
+        $this->assertCount(2, $jobs);
+        $this->assertEquals($jobs[0]['job']->post->id, $postTwo->id);
+        $this->assertEquals($jobs[1]['job']->post->id, $postOne->id);
     }
 
     public function test_actions_invalid_fields()
